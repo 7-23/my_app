c40dd650a8fc9b789b719bf7168a7cc0b5a1085a
unknown <dmitrii.lapshin.2017@mail.ru>
2021-01-27 22:05:04 +0300

    added store and refactoring state

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
-import {addPost} from './redux/state';
-import { rerenderEntireTree } from './render';
+import store from './redux/state';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-rerenderEntireTree(state);
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <BrowserRouter>
+    <React.StrictMode>
+      <App 
+        state={store.getState()}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText}
+        addMessage={store.addMessage.bind(store)}
+        updateNewMessageText={store.updateNewMessageText}
+        />
+    </React.StrictMode>
+    </BrowserRouter>,
+    document.getElementById('root'));  
+}
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,63 +1,77 @@
-import {rerenderEntireTree} from "../render";
-
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-            { id: 2, message: 'It\'s cool', likesCount: 11 }
-        ],
-        newPostText: 'it-kamasutra.com'
-
-        
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+                { id: 2, message: 'It\'s cool', likesCount: 11 }
+            ],
+            newPostText: 'it-kamasutra.com'
+    
+            
+        },
+        messagesPage: {
+            dialogs: [
+                { id: 1, name: 'Dmitrii', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoUPnIvTEoBZ_kjIuLl2OO-zlMZsCRFxWptQ&usqp=CAU"'},
+                { id: 2, name: 'Den', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvlWTEHQYhtXFkWHqAQOaQs2m3hqY78t4sNg&usqp=CAU' },
+                { id: 3, name: 'Jeka', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyw9Q7cEeqOOBgsn0fWAp-SaM0yxHtH0EneQ&usqp=CAU' }
+            ],
+            messages: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'Yo' },
+                { id: 3, message: 'Yapp' }
+            ],
+            newMessageText: 'samuray'
+        }
     },
-    messagesPage: {
-        dialogs: [
-            { id: 1, name: 'Dmitrii', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoUPnIvTEoBZ_kjIuLl2OO-zlMZsCRFxWptQ&usqp=CAU"'},
-            { id: 2, name: 'Den', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvlWTEHQYhtXFkWHqAQOaQs2m3hqY78t4sNg&usqp=CAU' },
-            { id: 3, name: 'Jeka', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyw9Q7cEeqOOBgsn0fWAp-SaM0yxHtH0EneQ&usqp=CAU' }
-        ],
-        messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'Yo' },
-            { id: 3, message: 'Yapp' }
-        ],
-        newMessageText: 'samuray'
+
+    getState() {
+        return this._state;
+    },
+
+    _callSubscriber() {
+        console.log('State changed')
+    },
+
+    addPost() {
+    
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0 
+        };
+    
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber();
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber();
+    },
+
+    addMessage() {
+        let newMessage = {
+            id: 4,
+            message: this._state.messagesPage.newMessageText
+        };
+    
+        this._state.messagesPage.messages.push(newMessage);
+        this._state.messagesPage.newMessageText = '';
+        this._callSubscriber();
+    },
+
+    updateNewMessageText(newText) {
+        this._state.messagesPage.newMessageText = newText;
+        this._callSubscriber();
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
-};
-
-window.state = state;
-
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0 
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
 
-export let addMessage = () => {
-    let newMessage = {
-        id: 4,
-        message: state.messagesPage.newMessageText
-    };
+export default store;
 
-    state.messagesPage.messages.push(newMessage);
-    state.messagesPage.newMessageText = '';
-    rerenderEntireTree(state);
-}
-
-export let updateNewMessageText = (newText) => {
-    state.messagesPage.newMessageText = newText;
-    rerenderEntireTree(state);
-}
-
-export default state;
+window.store = store;
