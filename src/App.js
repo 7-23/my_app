f504f8a67cf5799db1a49bf2d964b782eae619dd
unknown <dmitrii.lapshin.2017@mail.ru>
2021-01-28 18:47:22 +0300

    added dispatch

--- src/App.js
+++ src/App.js
@@ -18,13 +18,11 @@ const App = (props) => {
       <div className='app-wrapper-content'>
         <Route path='/profile' render={() => <Profile
           profilePage={props.state.profilePage}
-          addPost={props.addPost}
-          updateNewPostText={props.updateNewPostText}
+          dispatch={props.dispatch}
            />} />
         <Route path='/dialogs' render={() => <Dialogs
           state={props.state.messagesPage}
-          addMessage={props.addMessage}
-          updateNewMessageText={props.updateNewMessageText}
+          dispatch={props.dispatch}
         />} />
         <Route path='/news' component={News} />
         <Route path='/music' component={Music} />
--- src/redux/state.js
+++ src/redux/state.js
@@ -6,12 +6,12 @@ let store = {
                 { id: 2, message: 'It\'s cool', likesCount: 11 }
             ],
             newPostText: 'it-kamasutra.com'
-    
-            
+
+
         },
         messagesPage: {
             dialogs: [
-                { id: 1, name: 'Dmitrii', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoUPnIvTEoBZ_kjIuLl2OO-zlMZsCRFxWptQ&usqp=CAU"'},
+                { id: 1, name: 'Dmitrii', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoUPnIvTEoBZ_kjIuLl2OO-zlMZsCRFxWptQ&usqp=CAU"' },
                 { id: 2, name: 'Den', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvlWTEHQYhtXFkWHqAQOaQs2m3hqY78t4sNg&usqp=CAU' },
                 { id: 3, name: 'Jeka', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyw9Q7cEeqOOBgsn0fWAp-SaM0yxHtH0EneQ&usqp=CAU' }
             ],
@@ -24,50 +24,49 @@ let store = {
         }
     },
 
-    getState() {
-        return this._state;
-    },
-
     _callSubscriber() {
         console.log('State changed')
     },
 
-    addPost() {
-    
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0 
-        };
-    
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber();
-    },
 
-    updateNewPostText(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber();
-    },
 
-    addMessage() {
-        let newMessage = {
-            id: 4,
-            message: this._state.messagesPage.newMessageText
-        };
-    
-        this._state.messagesPage.messages.push(newMessage);
-        this._state.messagesPage.newMessageText = '';
-        this._callSubscriber();
-    },
-
-    updateNewMessageText(newText) {
-        this._state.messagesPage.newMessageText = newText;
-        this._callSubscriber();
+    getState() {
+        return this._state;
     },
 
     subscribe(observer) {
         this._callSubscriber = observer;
+    },
+
+
+
+    dispatch(action) {
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+    
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber();
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+        this._callSubscriber();
+        } else if (action.type === 'ADD-MESSAGE') {
+            let newMessage = {
+                id: 4,
+                message: this._state.messagesPage.newMessageText
+            };
+    
+            this._state.messagesPage.messages.push(newMessage);
+            this._state.messagesPage.newMessageText = '';
+            this._callSubscriber();
+        } else if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
+            this._state.messagesPage.newMessageText = action.newText;
+        this._callSubscriber();
+        }
     }
 }
 
