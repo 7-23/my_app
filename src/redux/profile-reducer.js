b9fa123878ef4415fd1d6a342757f9a408adf281
unknown <dmitrii.lapshin.2017@mail.ru>
2021-01-29 11:30:22 +0300

    refactoring profile-reducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,21 +2,24 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const profileReducer = (state, action) => {
-    if (action.type === ADD_POST) {
-        let newPost = {
-            id: 5,
-            message: state.newPostText,
-            likesCount: 0
-        };
-
-        state.posts.push(newPost);
-        state.newPostText = '';
-        
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newPostText;
-    }
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likesCount: 0
+            };
     
-    return state;
+            state.posts.push(newPost);
+            state.newPostText = '';
+
+            return state;
+        case   UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newPostText;
+            return state;
+        default:
+            return state;
+    }
 }
 
 export default profileReducer;
