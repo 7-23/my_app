78a653d0c58e99a05ce642df574c0ab9c8ae79a3
unknown <dmitrii.lapshin.2017@mail.ru>
2021-01-29 11:38:58 +0300

    refactoring dialogs-reducer

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -2,19 +2,22 @@ const SEND_MESSAGE = 'SEND-MESSAGE';
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 
 const dialogsReducer = (state, action) => {
-    if (action.type === SEND_MESSAGE) {
-        let body = {
-            id: 4,
-            message: state.newMessageBody
-        };
-        state.messages.push(body);
-        state.newMessageBody = '';
-        
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-        state.newMessageBody = action.body;
-    
+    switch (action.type) {
+        case SEND_MESSAGE:
+            let body = {
+                id: 4,
+                message: state.newMessageBody
+            };
+            state.messages.push(body);
+            state.newMessageBody = '';
+            return state;
+
+        case UPDATE_NEW_MESSAGE_BODY:
+            state.newMessageBody = action.body;
+            return state;
+
+        default: return state;
     }
-    return state;
 }
 
 export default dialogsReducer;
